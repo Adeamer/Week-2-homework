2b5c69693cc918fd7eff28c6df0e85dd87c79c99
Elziekio <anthony.deamer@hotmail.com>
2021-06-23 16:45:31 +1000

    third commit

--- Assets/js/menu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myNavigation");
-    if (x.className === "Navigation") {
-      x.className += " responsive";
-    } else {
-      x.className = "Navigation";
-    }
-  }
